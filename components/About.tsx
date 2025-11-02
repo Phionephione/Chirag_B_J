acd7ba03014c4eff6368e1c00fe84525ae6204a6
CHIRAG B J <ss.chiragbj.10b@gmail.com>
2025-11-02 22:01:00 +0530

    Remove unnecessary classes from About component

--- components/About.tsx
+++ components/About.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import Section from './Section';
 import { PERSONAL_INFO } from '../constants';
@@ -6,7 +5,7 @@ import { PERSONAL_INFO } from '../constants';
 const About: React.FC = () => {
   return (
     <Section title="01. About Me">
-      <p className="text-gray-300 text-lg leading-relaxed max-w-4xl text-center md:text-left mx-auto md:mx-0">
+      <p className="text-gray-300 text-lg leading-relaxed max-w-4xl">
         {PERSONAL_INFO.about}
       </p>
     </Section>
